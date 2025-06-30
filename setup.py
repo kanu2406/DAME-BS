b629cbfbddef39d0e7f2bedd686b7f164dc70c5b
Kanupriya Jain <85952221+kanu2406@users.noreply.github.com>
2025-06-30 17:14:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dame_ts",
+    name="dame_bs",
     version="0.1.0",
-    description="DAME-TS: Differentially Private Mean Estimation via Ternary Search",
+    description="DAME-BS: Differentially Private Mean Estimation via Binary Search",
     author="Kanupriya Jain",
     author_email="k.jain@criteo.com",
-    url="https://github.com/kanu2406/DAME-TS",  
+    url="https://github.com/kanu2406/DAME-BS",  
     packages=find_packages(),
     install_requires=[
         "numpy",
